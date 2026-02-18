80cd7883a70d3e94bcf29da42a4a619434932716
Elinasu001 <141826266+Elinasu001@users.noreply.github.com>
2026-02-18 21:15:15 +0900

    Revert "Revert "feat: /api/auth/send - 휴대폰 번호 입력 후 인증 번호 발송""

---
+++ src/main/java/com/mobilefactory/lotto/auth/model/service/AuthService.java
@@ -0,0 +1,11 @@
+package com.mobilefactory.lotto.auth.model.service;
+
+import com.mobilefactory.lotto.auth.model.dto.AuthResponse;
+import com.mobilefactory.lotto.auth.model.dto.SendAuthCodeRequest;
+
+public interface AuthService {
+	// 인증번호 발송
+    AuthResponse sendAuthCode(SendAuthCodeRequest request);
+    // 인증번호 검증
+    AuthResponse verifyAuthCode(String phoneNumber, String authCode);
+}
